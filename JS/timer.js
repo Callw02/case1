cc1b68c36234e85553073f32f068d5626fedebb5
Callw02 <muficcalle@gmail.com>
2023-10-02 13:40:27 +0200

    tried some timer stuff

--- JS/timer.js
+++ JS/timer.js
@@ -1,36 +1,28 @@
-let timeLeft = "1203";
+let timeLeft = "12";
 
-function timer(timeLeft){
-    const timerElement = document.querySelector("#try"); 
+function timer(timeLeft){   
     let timer = setInterval(function(){
-
+      let timerElement = document.querySelector("#circleContainer")
         let minutes = Math.floor(timeLeft / 60);
         let seconds = timeLeft % 60;
-
         let formattedMinutes = minutes.toString().padStart(2, '0');
         let formattedSeconds = seconds.toString().padStart(2, '0');
-    
+
+        const setTime = formattedMinutes + formattedSeconds;
+        const startTime = Date.now();
+        const futureTimer = startTime + setTime;
+
         // Update the timer display
         timerElement.textContent = `${formattedMinutes}:${formattedSeconds}`;
-    
         console.log(`${formattedMinutes}:${formattedSeconds}`);
         timeLeft--;
-       
-
-        timerElement.textContent = `${formattedMinutes}:${formattedSeconds}`;
-        console.log(timeLeft);
-      
-        if (timeLeft >= 0) {
-          // Decrease the border width proportionally
-          
-          
-        }
+        
         if(timeLeft === 0){
             clearInterval(timer);
             console.log("done!");
-            document.querySelector("#try").textContent = "Enjoy!"
+            document.querySelector("#circleContainer").textContent = "Enjoy!"
         }
-    }, 100)
+    }, 1000)
 }
 
 timer(timeLeft);
